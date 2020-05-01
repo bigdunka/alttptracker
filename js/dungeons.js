cdcd06886af1e4864e2e9f8f9b5ef03db3f4c686
bigdunka <31429479+bigdunka@users.noreply.github.com>
2020-04-30 22:20:27 -0500

    Build 1.5.019 043020
    Logical EP Keys issue

--- js/dungeons.js
+++ js/dungeons.js
@@ -470,7 +470,15 @@
 		} else {
 			if (flags.dungeonitems === 'F') {
 				if (items.bigkey0) {
-					chests[5] = 'A';
+					if (flags.enemyshuffle === 'N' && items.bow === 0) {
+						chests[5] = 'U';
+					} else {
+						if (items.lantern || (items.firerod && flags.itemplacement === 'A')) {
+							chests[5] = (dungeoncheck === 'available' ? 'A' : 'P');
+						} else {
+							chests[5] = (dungeoncheck === 'available' ? 'DA' : 'DP');
+						}
+					}
 				}
 			} else {
 				if (flags.enemyshuffle === 'N' && items.bow === 0) {
