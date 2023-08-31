e44d942920d10575dffb0e37ccce6031791f36ed
bigdunka <31429479+bigdunka@users.noreply.github.com>
2023-08-31 10:14:34 -0500

    Build 2.0.028 083123

--- js/util.js
+++ js/util.js
@@ -1,7 +1,7 @@
 (function (window) {
 	'use strict';
 
-	window.buildString = "20024";
+	window.buildString = "20028";
 
 	// based on https://github.com/medialize/URI.js/blob/gh-pages/src/URI.js
 	window.uri_query = memoize(function () {
